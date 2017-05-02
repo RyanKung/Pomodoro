f1aa5848f6b0b60e165db42ceb68233cad6381a0
Ryan.K <ryankung@ieee.org>
2017-05-02 15:05:47 +0800

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Ryan Kung on 2/5/2017.
+//  Copyright © 2017 Ryan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Ryan Kung on 2/5/2017.
+//  Copyright © 2017 Ryan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
